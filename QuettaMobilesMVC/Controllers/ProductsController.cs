0b99b5b5b4f0fa76a76600b94afc95ddfaeaa8de
muzam <muzam@DEVELOPMENT>
2024-09-02 21:05:59 +0500

    Add project files.

---
+++ QuettaMobilesMVC/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuettaMobilesMVC.Services;
+
+namespace QuettaMobilesMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+    }
+}
